e5d7eccf6da3fc8e11aa94d10398ffe6f764666a
janagambharath <90457142+janagambharath@users.noreply.github.com>
2025-07-20 12:07:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Bharath", 101, 91.5);
+        Student s2 = new Student("Neeraja", 102, 95.0);
+        Student s3 = new Student("Rahul", 103, 84.2);
+
+        s1.displayDetails();
+        s2.displayDetails();
+        s3.displayDetails();
+    }
+}
